c6a92ff66febb35ffa083b71441d31633ef6bca8
Shao-Lin <baulin2005@bk.ru>
2024-10-31 15:05:39 +0300

    некорректро отображалось модальное окно

--- src/View/viewingTheNews.js
+++ src/View/viewingTheNews.js
@@ -1,8 +1,8 @@
 const viewModal = (state, btn) => {
   const title = document.querySelector('.modal-title');
   const descriptionText = document.querySelector('.text-break');
-  descriptionText.textContent = `${state.feedsAndPosts.posts[btn.getAttribute('data-btn-id')].description}`;
-  title.textContent = `${state.feedsAndPosts.posts[btn.getAttribute('data-btn-id')].title}`;
+  descriptionText.textContent = `${state.feedsAndPosts.posts[btn.getAttribute('data-btn-id') - 1].description}`;
+  title.textContent = `${state.feedsAndPosts.posts[btn.getAttribute('data-btn-id') - 1].title}`;
 };
 const viewedContent = (state, btn) => {
   const a = btn.previousElementSibling;
